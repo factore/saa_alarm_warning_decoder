41508715664986a28f38000e99e5f39c6f601008
Andrew Noble <aenoble@gmail.com>
2017-08-04 11:42:01 -0400

    Load decoders.

--- lib/saa_alarm_warning_decoder.rb
+++ lib/saa_alarm_warning_decoder.rb
@@ -1,4 +1,6 @@
 require "saa_alarm_warning_decoder/version"
+require "saa_alarm_warning_decoder/alarms_decoder"
+require "saa_alarm_warning_decoder/warnings_decoder"
 
 module SaaAlarmWarningDecoder
   # Your code goes here...
